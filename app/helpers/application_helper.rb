796e656faa567e2f07e6e171df0fcf1889a9238b
Vitor Nascimento Arizona <vitor.nascimento.jr@gmail.com>
2013-04-01 18:26:16 -0300

    resolvido problema do primeiro cadastro do grupo root

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   def avatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=39"
+  	if user
+    	gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    	"http://gravatar.com/avatar/#{gravatar_id}.png?s=39"
+    end
   end
 end
