2e0714962bcf7a62c4110ac9f329fbad9095d290
Vitinho <vitor.nascimento.jr@gmail.com>
2013-04-22 23:08:51 -0300

    Login and emails ajust

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,16 @@ Turismoapp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  # SMTP Email configuration
   config.action_mailer.default_url_options = { :host => 'turismoapp.amazon' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address =>              'smtp.gmail.com',
+    :port =>                 587,
+    :domain =>               'gmail.com',
+    :user_name =>            'vitordipreza@gmail.com',
+    :password =>             'euvitinho',
+    :authentication =>       'plain',
+    :enable_starttls_auto => true  }
     
 end
